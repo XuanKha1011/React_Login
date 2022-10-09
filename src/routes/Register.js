b0033222a847fc6b55b6e8f4e95fa844bba3eb41
XuanKha <nguyenxuankhka5371@GMAIL.COM>
2022-10-09 18:20:52 +0700

    update CSS login

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import "./Register.css";
 
 function Register(props) {
+  const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmitRegister = (e) => {
+    e.preventDefault();
+    console.log(email);
+  };
+
   return (
-    <div>Register</div>
-  )
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="login-form" onSubmit={handleSubmitRegister}>
+        <label htmlFor="email">email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <button className="register-btn" type="submit">Submit</button>
+      </form>
+     
+    </div>
+  );
 }
 
-
-export default Register
-
+export default Register;
